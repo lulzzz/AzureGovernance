30fbb84e654df5c80226e40e6bc67136a1ba16c3
Felix Bodmer <felix.bodmer@outlook.com>
2018-12-14 07:20:56 +0100

    a

---
+++ TEC0002-TagExport.ps1
@@ -0,0 +1,168 @@
+﻿###############################################################################################################################################################
+# Exporting tags to an Excel document. 
+#
+# Error Handling: There is no error handling available in this pattern. 
+# 
+# Output:         None
+#
+# Requirements:   This is not working if the same tag is used with different cases, e.g. Test/test. This is because PowerShell is not case sensitive. 
+#                 Some Resource Types don't allow changes on the Tags if they are not running, this might block the execution of this script.                
+#
+# Template:       None
+#
+# Change log:
+# 1.0             Initial version 
+#
+###############################################################################################################################################################
+workflow TEC0002-TagExport
+{
+  $VerbosePreference ='Continue'
+
+  TEC0005-AzureContextSet
+  
+  InlineScript
+  {
+    # Create table
+    $Table = New-Object System.Data.Datatable
+    [void]$Table.Columns.Add('KeyName')
+    [void]$Table.Columns.Add('KeyRgName')
+    [void]$Table.Columns.Add('KeyType')
+
+    # Get Tags of Resources
+    $Resource = Get-AzureRmResource # | Where-Object {  $_.Name -like 'weu*' -or $_.Name -like 'roc*'}
+    $Counter = $Resource.Count
+    do
+    {
+      $Counter = $Counter - 1
+      $Resource.Name[$Counter]
+      $Tags = (Get-AzureRmResource -ResourceName $Resource.Name[$Counter] -ResourceGroupName $Resource.ResourceGroupName[$Counter]).Tags
+      # Add columns to table
+      if ($Tags.Length -ne 0)
+      {
+        foreach ($Tag in $Tags.Keys)
+        {
+          try
+          {
+            [void]$Table.Columns.Add($Tag)
+          }
+          catch
+          {
+            'Column already existing'
+          }
+        }
+      }
+      $Row = $Table.NewRow()
+      $Row.KeyName = $Resource.Name[$Counter]
+      $Row.KeyRgName = $Resource.ResourceGroupName[$Counter]
+      $Row.KeyType = $Resource.ResourceType[$Counter]
+
+      if ($Resource.Tags[$Counter] -ne $null)
+      {
+        foreach ($Tag in $Tags.GetEnumerator())
+        {
+          # Convert key/value pair to string
+          $Name = $Tag.Name.ToString()
+          $Value = $Tag.Value.ToString()
+          Set-Variable -Name Var3 -Value $Name  
+          $Row.(Get-Variable -Name Var3 -ValueOnly) = $Value
+        }
+      }
+      $Table.Rows.Add($Row)
+    } until ($Counter -eq 0)
+    Write-Verbose -Message ("TEC0002-ResourceTagsRetrieved" + ($Table | Out-String))
+
+    # Get Tags of Resource Groups
+    $Resource = Get-AzureRmResourceGroup # | Where-Object {  $_.ResourceGroupName -like 'weu*' -or $_.Name -like 'roc*'}
+    $Counter = $Resource.Count
+    do
+    {
+      # Add columns to table
+      $Counter = $Counter - 1
+      if ($Resource[$Counter].Tags.Length -ne 0)
+      {
+        foreach ($Tag in $Resource.Tags[$Counter].Keys)
+        {
+          try
+          {
+            [void]$Table.Columns.Add($Tag)
+          }
+          catch
+          {
+            'Column already existing'
+          }
+        }
+      }
+      $Row = $Table.NewRow()
+      $Row.KeyName = $Resource.ResourceGroupName[$Counter]
+      $Row.KeyRgName = $Resource.ResourceGroupName[$Counter]
+      $Row.KeyType = 'ResourceGroup'
+
+      if ($Resource.Tags[$Counter] -ne $null)
+      {
+        foreach ($Tag in $Resource.Tags[$Counter].GetEnumerator())
+        {
+          # Convert key/value pair to string
+          $Name = $Tag.Name.ToString()
+          $Value = $Tag.Value.ToString()
+          Set-Variable -Name Var3 -Value $Name  
+          $Row.(Get-Variable -Name Var3 -ValueOnly) = $Value
+        }
+      }
+      $Table.Rows.Add($Row)
+    } until ($Counter -eq 0)
+    Write-Verbose -Message ("TEC0002-ResourceGroupTagsRetrieved: " + ($Table | Out-String))
+    
+    # Map drive to save Tags in Excel
+    $StorageAccountName = Get-AutomationVariable -Name 'VAR-AUTO-StorageAccountName' -Verbose:$false
+    $StorageAccount = Get-AzureRmResource | Where-Object {$_.Name -eq $StorageAccountName}
+    $StorageAccountKey = (Get-AzureRMStorageAccountKey -ResourceGroupName $StorageAccount.ResourceGroupName -Name $StorageAccount.Name).Value[0]
+    $StorageAccountKey = ConvertTo-SecureString -String $StorageAccountKey -AsPlainText -Force
+    $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$StorageAccountName", $StorageAccountKey
+    $Result = New-PSDrive -Name T -PSProvider FileSystem -Root "\\$StorageAccountName.file.core.windows.net\tagexport" -Credential $Credential -Persist
+    Write-Verbose -Message ('TEC0002-DriveTMapped: ' + ($Result | Out-String))
+    
+    $Table | export-csv T:\Tags.csv -noType 
+    Remove-Item T:\Tags.xls -Force -ErrorAction SilentlyContinue
+    Write-Verbose -Message ("TEC0002-CsvExported")
+
+    # Set input and output path
+    $inputCSV = 'T:\Tags.csv'
+    $outputXLSX = 'T:\Tags.xlsx'
+    Write-Verbose '1'
+
+    # Create a new Excel Workbook with one empty sheet
+    $excel = New-Object -ComObject excel.application 
+    $workbook = $excel.Workbooks.Add(1)
+    Write-Verbose '2'
+    $worksheet = $workbook.worksheets.Item(1)
+    Write-Verbose '3'
+    
+    # Build the QueryTables.Add command - QueryTables does the same as when clicking "Data -> From Text" in Excel
+    $TxtConnector = ('TEXT;' + $inputCSV)
+    $Connector = $worksheet.QueryTables.add($TxtConnector,$worksheet.Range('A1'))
+    $query = $worksheet.QueryTables.item($Connector.name)
+    Write-Verbose '4'
+    # Set the delimiter (, or ;) according to your regional settings
+    $query.TextFileOtherDelimiter = ',' #$Excel.Application.International(5)
+    Write-Verbose '5'
+    # Set the format to delimited and text for every column - A trick to create an array of 2s is used with the preceding comma
+    $query.TextFileParseType  = 1
+    $query.TextFileColumnDataTypes = ,2 * $worksheet.Cells.Columns.Count
+    $query.AdjustColumnWidth = 1
+    Write-Verbose '6'
+    # Execute & delete the import query
+    $query.Refresh()
+    Write-Verbose '7'
+    $query.Delete()
+    Write-Verbose '8'
+    # Save & close the Workbook as XLSX. Change the output extension for Excel 2003
+    $Workbook.SaveAs($outputXLSX)
+    Write-Verbose '9'
+    $excel.Quit()
+    Write-Verbose -Message ("TEC0002-TagsExportedToExcel")
+    
+    # Clean up
+    #Remove-Item T:\Tags.csv
+    Remove-PSDrive -Name T -Force
+  }
+}
---
+++ TEC0003-TagImport.ps1
@@ -0,0 +1,85 @@
+﻿###############################################################################################################################################################
+# Exporting tags from an Excel document. This will overwrite all existing tags. 
+#
+# Error Handling: There is no error handling available in this pattern. 
+# 
+# Output:         None
+#
+# Requirements:   This requires an Excel document with a structure as created by T0002
+#
+# Template:       None
+#
+# Change log:
+# 1.0             Initial version 
+#
+###############################################################################################################################################################
+workflow TEC0003-TagImport
+{
+  $VerbosePreference ='Continue'
+
+  TEC0005-SetAzureContext
+  
+  InlineScript
+  {
+    # Map drive to save Tags in Excel
+    $StorageAccountName = Get-AutomationVariable -Name 'VAR-AUTO-StorageAccountName' -Verbose:$false
+    $StorageAccount = Get-AzureRmResource | Where-Object {$_.Name -eq $StorageAccountName}
+    $StorageAccountKey = (Get-AzureRMStorageAccountKey -ResourceGroupName $StorageAccount.ResourceGroupName -Name $StorageAccount.Name).Value[0]
+    $StorageAccountKey = ConvertTo-SecureString -String $StorageAccountKey -AsPlainText -Force
+    $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$StorageAccountName", $StorageAccountKey
+    New-PSDrive -Name T -PSProvider FileSystem -Root "\\$StorageAccountName.file.core.windows.net\tagexport" -Credential $Credential -Persist
+    
+    # Read tags from Excel and write to Azure
+    $Excel = new-object -com excel.application
+    $Workbook = $Excel.workbooks.open('T:\Tags.xls')
+    $Sheet = $Workbook.Sheets.Item(1)
+
+    $RowNumber = 2
+    do
+    {
+      # Get entries for one resource - essentially a data row in the table with the header row
+      $ColumnNumber = 1
+      $TagsExcel = @{}
+      do
+      {
+        # Name (row/column)
+        $Name = $Sheet.Cells.Item(1,$ColumnNumber).Value2
+        if ($Name.Length -eq 0) {break} 
+
+        # Value (row/column)
+        $Value = $Sheet.Cells.Item($RowNumber,$ColumnNumber).Value2
+
+        # Add Name/Value pair to hash table - but only if there is a value in the Tag   
+        if ($Value -ne $null)
+        {
+          $TagsExcel.Add($Name, $Value)
+        }
+        $ColumnNumber++      
+      }
+      while ($Name.Length -gt 0)
+
+      if ($TagsExcel.KeyName.Length -eq 0) {break}
+      $TagsExcel
+
+      #$TagsNew = @{}
+      #$TagsExcel | Foreach-Object {$TagsNew[$_.Name] = $_.Value }
+  
+      $TagsNew = $TagsExcel.Clone()
+      $TagsNew.Remove('KeyName')
+      $TagsNew.Remove('KeyType')
+      $TagsNew.Remove('KeyRgName')
+      if ($TagsExcel.KeyType -eq 'ResourceGroup')
+      {
+        Set-AzureRmResourceGroup -Name $TagsExcel.KeyRgName -Tag $TagsNew
+      }
+      else
+      {
+        Set-AzureRmResource -Name $TagsExcel.KeyName -ResourceGroupName $TagsExcel.KeyRgName -ResourceType $TagsExcel.KeyType -Tag $TagsNew -Force
+      }
+      $RowNumber++
+    }
+    while ($TagsExcel.KeyName.Length -gt 0)
+
+    $Excel.Workbooks.Close()
+  }
+}
