f34241db587420bc7e6942d96e74480a2d826c2d
Felix Bodmer <felix.bodmer@bluewin.ch>
2019-01-17 07:35:06 +0100

    Update SOL0012-ResourceGroupNew.ps1

--- SOL0012-ResourceGroupNew.ps1
+++ SOL0012-ResourceGroupNew.ps1
@@ -48,15 +48,26 @@ workflow SOL0012-ResourceGroupNew
   # Deploy Template
   #
   #############################################################################################################################################################
-  New-AzureRmDeployment -Location westeurope -ResourceGroupNameIndividual test -Region westeurope -SubscriptionName Core-co -ApplicationId App-1 -CostCenter 24-1234 -Budget 222 `
-                        -Contact felix.bodmer@outlook.com -TemplateUri https://raw.githubusercontent.com/fbodmer/AzureGovernance/master/SOL0012-ResourceGroupNew.json 
+  New-AzureRmDeployment -Location westeurope -ResourceGroupNameIndividual aaa -Region westeurope -SubscriptionName Core-co -ApplicationId App-1 `
+                        -CostCenter 24-1234 -Budget 222 -Contact felix@outlook.com `
+                        -AadId ffea2e1f-0679-454f-8820-65a0186028b8 -BuiltInRoleType Reader `
+                                     -RoleNameGuid 41111111-1111-1111-1111-111111111111 `
+                        -TemplateUri https://raw.githubusercontent.com/fbodmer/AzureGovernance/master/SOL000-RgNew.json 
+
+
+  # Create Resource Group
+  New-AzureRmDeployment -Location westeurope -ResourceGroupNameIndividual test -Region westeurope -SubscriptionName Core-co -ApplicationId App-1 `
+                        -CostCenter 24-1234 -Budget 222 -Contact felix@outlook.com `
+                        -TemplateUri https://raw.githubusercontent.com/fbodmer/AzureGovernance/master/PAT0000-ResourceGroupNew.json 
 
 
 
-  New-AzureRmDeployment -Location westeurope -principalId ffea2e1f-0679-454f-8820-65a0186028b8 -builtInRoleType Reader -roleNameGuid 11111111-1111-1111-1111-111111111111 -TemplateUri https://raw.githubusercontent.com/fbodmer/AzureGovernance/master/SOL0012-ResourceGroupNewPolicy.json 
+  # Assign RBAC
+  New-AzureRmDeployment -Location westeurope -ResourceGroupName weu-co-rsg-test-99 -AadId ffea2e1f-0679-454f-8820-65a0186028b8 -BuiltInRoleType Reader `
+                        -RoleNameGuid 21111111-1111-1111-1111-111111111111 -Region westeurope `
+                        -TemplateUri https://raw.githubusercontent.com/fbodmer/AzureGovernance/master/PAT0000-ResourceGroupNew.json
 
-
-
+  New-AzureRmResourceGroupDeployment 
 
 
 }
